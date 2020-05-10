bd94175339c40c32df0db84529c77039d3bb2cc4
ergün yılmaz <ergunyilmaz176@gmail.com>
2020-05-10 03:25:09 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using NetWenApiStudentAntreman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetWenApiStudentAntreman.Controllers
+{
+    public class HomeController : ApiController
+    {
+        StudentDBEntities db = new StudentDBEntities();
+        public IEnumerable<Student> Get()
+        {
+            return db.Students.ToList();
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            Student student = db.Students.FirstOrDefault(x => x.ID == id);
+            if (student!=null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, student);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"{id} numaralı bir öğrenci bulunamadı...");
+            }
+        }
+    }
+}
